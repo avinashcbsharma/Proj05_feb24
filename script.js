a991e41bab9e77bb1d093237cdaf65c6d6deea1f
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-17 14:52:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 /** @format */
 
 let students = [
-  { id: 1, name: "john", age: "18", marks: 80 },
-  { id: 2, name: "jack", age: "20", marks: 85 },
-  { id: 3, name: "karen", age: "19", marks: 35 },
-];
+{ id:1, name: "john", age:"18", marks:80 }, 
+{ id:2, name: "jack", age:"20", marks:85 }, 
+{ id:3, name: "karen", age:"19", marks:35 }
 
-function PrintStudentswithMap() {
+]
+
   
+// Function to print students with marks over 50 using map
+
+  function printStudentsByMap() {
     console.log("\nStudents above 50 marks using map()");
     const StudentsAbove50 = students
       .filter(student => student.marks > 50)
@@ -15,39 +18,52 @@ function PrintStudentswithMap() {
     
     console.log(StudentsAbove50);  
 }
-function PrintStudentswithMap();
+printStudentsByMap();
 
-function PrintStudentsbyForEach() {
-   console.log("\nStudents above 50 marks using forEach()");
+//Print all students alongwith info.
+ function PrintStudentbyForEach(){
+    console.log("\nStudents above 50 marks using forEach()");
 
     const StudentsAbove50 = students.filter(student => student.marks > 50); 
 
     StudentsAbove50.forEach(studentDetail => console.log(studentDetail));
 }
+PrintStudentbyForEach();
 
-function addData() {
- console.log("\nStudents List After Adding new record");
+//Adding new object into array
+
+addData = (rec)=>{
+    console.log("\nStudents List After Adding new record");
     const moreStudent = {id:4,name:"susan",age:"20",marks:45};
     const studentInfo = rec.push(moreStudent);
     
     rec.forEach(studentInfo => console.log(studentInfo));
+
 }
 addData(students);
 
-function removeFailedStudent() {
-  console.log("\nStudents List After deleting failed record");
+//filtering failed students
+
+removeFailedStudent =(obj)=>{
+    console.log("\nStudents List After deleting failed record");
     const condition = obj => obj.marks < 50;
     const newobj = obj.filter(obj => !condition(obj));
     console.log(newobj);
+   
 }
 removeFailedStudent(students);
 
+
+//concating Arrays
+//other Array of objects
 const array =[
     {id:11, name: 'Alice', age :"20", marks: 45 }, 
     { id:12, name: "Eva", age:"23", marks:65 }, 
     { id:13, name: "Laren", age:"29", marks:85 }
-function concatenateArray() {
-  console.log("\nStudents List After concatenate");
-  return arr1.concat(arr2);
+]
+ConcatenateArray = (arr1,arr2) =>{
+    const arr = arr1.concat(arr2);
+    
 }
+console.log("\nStudents List After concatenate");
 console.log(ConcatenateArray(students,array));
